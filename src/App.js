037524bbd29b279db0a3f33c6b1be1b54487d589
Valentin Mandiev <110761981+valentinmandiev@users.noreply.github.com>
2023-05-15 12:50:18 +0300

    added crypto-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Table, Image } from "react-bootstrap";
+import "./App.css";
 
-function App() {
+const CryptoTable = () => {
+  const [cryptoData, setCryptoData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        "https://api.coingecko.com/api/v3/coins/markets",
+        {
+          params: {
+            vs_currency: "usd",
+            order: "market_cap_desc",
+            per_page: 18,
+            page: 1,
+          },
+        }
+      );
+      setCryptoData(response.data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Table className="table" striped bordered hover>
+      <thead className="thead">
+        <tr>
+          <th>Name</th>
+          <th>Price (USD)</th>
+          <th>Crypto-Logo</th>
+        </tr>
+      </thead>
+      <tbody>
+        {cryptoData.map((coin) => (
+          <tr key={coin.id}>
+            <td>{coin.name}</td>
+            <td>${coin.current_price.toFixed(2)}</td>
+            <td>
+              <Image src={coin.image} alt={coin.name} height="30" />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   );
-}
+};
 
-export default App;
+export default CryptoTable;
